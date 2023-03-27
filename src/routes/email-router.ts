44b064a9ed8b162de6263ac157cccce36263b2a5
Gennadii <atlantagenafes@gmail.com>
2023-03-27 17:08:24 +0300

    update the versel app

--- src/routes/email-router.ts
+++ src/routes/email-router.ts
@@ -6,6 +6,7 @@ export const emailRouter = Router({})
 emailRouter
     .post('/send',
         // authMiddleware,
+
         // body('login').trim().not().isEmpty().withMessage('enter input value in name field'),
         // body('password').trim().not().isEmpty().withMessage('enter input value in password field'),
         // body('login').isLength({max: 10, min: 3}).withMessage('login: maxLength: 10 minLength: 3'),
